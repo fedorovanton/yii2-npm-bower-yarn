4564ebc7777c951b43b2efa78fbf3e1bc6432274
Anton Fedorov <fedorovau2012@gmail.com>
2018-09-06 00:03:48 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -9,6 +9,7 @@ $config = [
     
     'aliases' => [
         '@bower'  => '@vendor/bower-asset',
+        '@npm'    => '@vendor/npm-asset',
         '@yarn'   => '@app/node_modules',
     ],
     'components' => [
